b8aa1eeed81cc66ec993c2e06f930b15e2beef3c
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-05-10 05:35:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+const team=players;
+const team1=players.slice();
+const cap1=person.slice();
+
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
-const team=players;
-const team1=players.slice();
-const cap1=person.slice();
